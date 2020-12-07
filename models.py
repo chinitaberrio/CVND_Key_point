d5dab89661f0f2794ceb1fbfe92c1f7d98662c3e
Julie Stephany Berrio Perez <jber4282@sydney.edu.au>
2020-12-07 14:10:21 +1100

    better model

--- models.py
+++ models.py
@@ -27,36 +27,59 @@ class Net(nn.Module):
         # output size (32, 110, 110)
         self.pool1 = nn.MaxPool2d(2, 2)
 
+        # batch norm
+        self.bn1 = nn.BatchNorm2d(32)
+
         # 10 output channels/feature maps, 5x5 square convolution kernel
         ## output size = (W-F)/S +1 = (110-5)/1 +1 = 106
-        self.conv2 = nn.Conv2d(32, 10, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
 
         # maxpool layer: pool with kernel_size=2, stride=2
         # output size (10, 53, 53)
         self.pool2 = nn.MaxPool2d(2, 2)
 
-        # 5 output channels/feature maps, 3x3 square convolution kernel
-        ## output size = (W-F)/S +1 = (53-3)/1 +1 = 51
-        self.conv3 = nn.Conv2d(10, 5, 3)
+        # batch norm
+        self.bn2 = nn.BatchNorm2d(64)
 
-        self.fc1 = nn.Linear(10 * 51 * 51, 1000)
+        # 10 output channels/feature maps, 5x5 square convolution kernel
+        ## output size = (W-F)/S +1 = (53-5)/1 +1 = 49
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        # maxpool layer: pool with kernel_size=2, stride=2
+        # output size (128, 24, 24)
+        self.pool3 = nn.MaxPool2d(2, 2)
+
+        # 5 output channels/feature maps, 3x3 square convolution kernel
+        ## output size = (W-F)/S +1 = (24-3)/1 +1 = 22
+        self.conv4 = nn.Conv2d(128, 256, 3)
+
+        # maxpool layer: pool with kernel_size=2, stride=2
+        # output size (256, 11, 11)
+        self.pool4 = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(256 * 11 * 11, 8000)
 
         self.fc1_drop = nn.Dropout(p=0.3)
 
-        self.fc2 = nn.Linear(1000, 136)
+        self.fc2 = nn.Linear(8000, 1000)
+
+        self.fc2_drop = nn.Dropout(p=0.3)
+
+        self.fc3 = nn.Linear(1000, 136)
 
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
-
+        x = self.pool1(self.bn1(F.relu(self.conv1(x))))
+        x = self.pool2(self.bn2(F.relu(self.conv2(x))))
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.pool4(F.relu(self.conv4(x)))
         x = x.view(x.size(0), -1)
-
         x = F.relu(self.fc1(x))
         x = self.fc1_drop(x)
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc2_drop(x)
+        x = self.fc3(x)
 
         # a modified x, having gone through all the layers of your model, should be returned
         return x
